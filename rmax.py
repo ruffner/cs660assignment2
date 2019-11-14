ca06e44aebe9b8f1b11d66b26ae7d92932383ef0
Matt Ruffner <mattruffner7@gmail.com>
2019-11-14 16:39:07 -0500

    rmax progress

--- rmax.py
+++ rmax.py
@@ -2,82 +2,153 @@ import gym
 import numpy as np
 import random
 
-class MDP:
-    def __init__(self, env, discount=1.0, iterThresh=0.00001):
-        self.U = {}
-        self.env = env
-        self.gamma = discount
-        self.theta = iterThresh
-        self.na = env.action_space.n
-        self.ns = env.observation_space.n
-        self.policy = np.ones([self.ns, self.na]) / self.na
-        #print('made blank policy: ', self.policy)
+
+env       = gym.make('FrozenLake-v0')
+theta     = 0.001
+gamma     = 0.5
+tk_thresh = 5
+
+
+class Policy:
+    def __init__(self):
+        self.policy = np.random.randint(0,
+                                        env.action_space.n,
+                                        size=[env.observation_space.n, env.action_space.n])
+        #self.policy = np.zeros([env.observation_space.n, env.action_space.n])
+        self.values = np.zeros(env.observation_space.n)
+        self.Rsa    = np.zeros([env.observation_space.n, env.action_space.n])
+        self.Nsas   = np.zeros([env.observation_space.n,
+                                env.action_space.n,
+                                env.observation_space.n])
+        self.known_arcs = 0
         
-    def getAction(self, s):
-        action_val = max(self.policy[s])
-        action_idx = [i for i, j in enumerate(self.policy[s]) if j == action_val]
-        if len(action_idx) > 1:
-            action_idx = action_idx[ round(random.random() * (len(action_idx)-1)) ]
-        return action_idx
-        
-    def evaluate(self):
-        V = np.zeros(self.ns)
+    def getRs(self):
+        return self.Rs
+
+    def getNsas(self):
+        return self.Nsas
+    
+    def getNsa(self):
+        return np.sum(self.Nsas, axis=2)
+
+    def getK(self):
+        return np.argwhere(self.getNsa() > tk_thresh)
+
+    def getPolicy(self):
+        return self.policy
+
+    def getValues(self):
+        return self.values
+    
+    def step(self, s, a, r, sp):
+        self.Nsas[s, a, sp] += 1
+        self.Rsa[s][a] += r
+
+    def selectAction(self, s):
+        actions = self.policy[s]
+        if np.amax(actions).size > 1:
+            return np.random.randint(0, np.amax(actions).size)
+        else:
+            return np.argmax(actions)
+
+    def iterate(self):
+        V = self.values
+        P, R = self.getMDP()
+
+
+        # ---------------------------------    evaluation
         while True:
             delta = 0
-            for s in range(self.ns):
-                v = 0
-                for a, a_prob in enumerate(self.policy[s]):
-                    for prob, next_state, reward, done in env.P[s][a]:
-                        v += a_prob * prob * (reward + self.gamma * V[next_state])
+            
+            # sum over s
+            for s in range(self.policy.shape[0]):
+                v = V[s]
+
+                # sum over s'
+                for sp in range(self.policy.shape[0]):
+                    a = self.selectAction(s)
+                    v += P[s, self.selectAction(s), sp] * ( R[s][a] + gamma * V[sp])
+
                 delta = max(delta, np.abs(v - V[s]))
                 V[s] = v
                 
-            if delta < self.theta:
+            if delta < theta:
                 break
-        return np.array(V)
 
-    def improve(self):
-        # evaluate current policy
-        v = self.evaluate()
+        print('evaluated, state utils are: ', v)
 
-        #print('evaluated, state utils are: ', v)
-
-        for s in range(self.ns):
-            row = np.zeros(self.na)
+        
+        # --------------------------------   improvement
+        
+        stable = True
+        while 
+        for s in range(self.policy.shape[0]):
+            row = np.zeros(env.action_space.n)
             for a, a_prob in enumerate(self.policy[s]):
-                for prob, next_state, reward, done in env.P[s][a]:
-                    row[a] += a_prob * prob * v[next_state]
+                for sp in range(self.policy.shape[0]):
+                    row[a] += a_prob * P[s][a][sp] * V[sp]
 
             # update policy with new action likelyhoods
             self.policy[s] = row
 
+    def getMDP(self):
+        P = np.zeros(self.Nsas.shape)
+        K = self.getK()
+        R = np.ones(self.getNsa().shape)
 
-# make two identical environments, one for planning and one
-# for executing actions and observing reward info to propogate
-# back into the planning mdp
+        for s in range(P.shape[0]):
+            for a in range(P.shape[1]):
+                for sp in range(P.shape[2]):
+                    for t in K:
+                        if t[0] == s and t[1] == a:
+                            P[s][a][sp] = self.Nsas[s][a][sp] / self.getNsa()[s][a]
+                        else:
+                            if s == sp:
+                                P[s][a][sp] = 1
+                            else:
+                                P[s][a][sp] = 0
+        for s in range(R.shape[0]):
+            for a in range(R.shape[1]):
+                for t in K:
+                    if not (t[0] == s and t[1] == a):
+                        R[s][a] = 1
+        return P, R
 
-#planEnv = gym.make('FrozenLake8x8-v0')
-#realEnv = gym.make('FrozenLake8x8-v0')
-planEnv = gym.make('FrozenLake-v0')
-realEnv = gym.make('FrozenLake-v0')
+    def should_replan(self):
+        K = self.getK()
+        if K.shape[0] > self.known_arcs:
+            self.known_arcs = K.shape[0]
+            return True
+        else:
+            return False
 
-# make MDPs for each of the two environments
-planMDP = MDP(planEnv)
-realMDP = MDP(realEnv)
+#######################################################
+#######################################################
+        
+episodes = 10
+pknown = Policy()
 
+for ep in range(episodes):
 
-done = False
+    s = env.reset()
 
-while not done:
-    env.render()
-    print('in state ', nextState)
+    print('starting episode, policy: ', pknown.policy)
     
-    action = knownPolicy.getAction(nextState)
-    print('  taking action ', action)
-    
-    nextState, reward, done, info = env.step(env.action_space.sample())
+    while True:
 
-    knownPolicy.improve()
+        a = pknown.selectAction(s)
 
-    print('new policy: ', knownPolicy.policy)
-env.render()
+        print('a is: ',a)
+        
+        # execute and observe in the real environment
+        sp, r, terminal, _ = env.step(a)
+        
+        pknown.step(s, a, r, sp)
+
+        if terminal or pknown.should_replan():
+            pknown.iterate()
+            break
+        
+        s = sp
+
+print('\n\n\nfinal policy: ', pknown.policy)
